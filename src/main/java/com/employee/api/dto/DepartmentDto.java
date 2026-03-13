0925e9fb2d0b01f7be95bbae5d25d0e8473427c4
nirey-l <lyr725@gmail.com>
2026-03-13 15:47:58 +0900

    mapper add

--- src/main/java/com/employee/api/dto/DepartmentDto.java
+++ src/main/java/com/employee/api/dto/DepartmentDto.java
@@ -1,14 +1,12 @@
 package com.employee.api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class DepartmentDto {
     private Long id;
     private String departmentName;
